53e8d5885dee3aa2ea7863dfad0e19e68c70d39d
hmsalamaga <hmsalamaga@gmail.com>
2021-11-27 12:30:01 +0100

    Fix regex for 7b

--- zad1/index.js
+++ zad1/index.js
@@ -1,7 +1,7 @@
 function validateSysPath2() {
     let element = document.getElementById('sys_path2')
-    let pattern = '[A-Za-z]:\\(windows|winnt|win|dos|msdos)((\\.*)?)';
-    let regex = new RegExp(pattern, 'ui');
+    let pattern = '^[A-Za-z]:\\\\(windows|winnt|win|dos|msdos)(\\\\.*)?$';
+    let regex = new RegExp(pattern, 'i');
     if (element.value.length === 0) {
         element.classList.remove('invalid');
         element.classList.remove('valid');
