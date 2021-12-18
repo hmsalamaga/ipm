632a4c0923c8a919e5efdb1a41d0bc60184c1d7f
hmsalamaga <hmsalamaga@gmail.com>
2021-12-18 11:39:33 +0100

    Reorganize files structure

---
+++ zad3/punkt_1/index.js
@@ -0,0 +1,37 @@
+let dragged;
+let index;
+let indexDrop;
+let list;
+
+document.addEventListener("dragstart", ({target}) => {
+		dragged = target;
+		list = target.parentNode.children;
+		
+		for(let i = 0; i < list.length; i += 1) {
+			if(list[i] === dragged){
+				index = i;
+			}
+		}
+});
+
+document.addEventListener("dragover", (event) => {
+		event.preventDefault();
+});
+
+document.addEventListener("drop", ({target}) => {
+	if(target.className == "element" && target !== dragged) {
+		dragged.remove(dragged);
+
+		for(let i = 0; i < list.length; i += 1) {
+			if(list[i] === target){
+				indexDrop = i;
+			}
+		}
+
+		if(index > indexDrop) {
+			target.before( dragged );
+		} else {
+			target.after( dragged );
+		}
+	}
+});
