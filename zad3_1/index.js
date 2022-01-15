e77ac85b354181ec7b04dd74730fa4cabbbbf8be
hmsalamaga <hmsalamaga@gmail.com>
2022-01-15 12:16:43 +0100

    Implement collisions detection

--- zad3_1/index.js
+++ zad3_1/index.js
@@ -1,33 +1,80 @@
-let dragged = null;
-let startLeft = null;
-let startTop = null;
+const boxHeight = 50;
+const boxWidth = 50;
 
-document.addEventListener("dragstart", ({target, offsetX, offsetY, clientX, clientY}) => {
+let dragged = null;
+let shiftLeft = null;
+let shiftTop = null;
+
+document.addEventListener("dragstart", ({target, clientX, clientY}) => {
 	dragged = target;
-	startLeft = (offsetX || clientX - $(target).offset().left);
-	startTop = (offsetY || clientY - $(target).offset().top);
+	const rect = dragged.getBoundingClientRect();
+	shiftLeft = clientX - rect.left;
+	shiftTop = clientY - rect.top;
 });
 
 document.addEventListener("dragover", (event) => {
 		event.preventDefault();
 });
 
-document.addEventListener("drop", ({target, offsetX, offsetY}) => {
-	if(target.id === 'platform') {
-		dragged.remove(dragged);
+document.addEventListener("drop", ({target, clientX, clientY, layerX, layerY}) => {
+	const pageX = clientX - shiftLeft;
+	const pageY = clientY - shiftTop;
 
-		dragged.style.top = (offsetY - startTop) + 'px';
-		dragged.style.left = (offsetX - startLeft) + 'px';
-
-		target.appendChild(dragged);
+	if(isTargetPlatform(target, pageX, pageY)) {
+			moveBox(layerX, layerY, target);
 	}
 });
 
-function randomColor() {
-	return Math.floor(Math.random()*16777215).toString(16);
+const isTargetPlatform = (target, pageX, pageY) => {
+	return target.id === 'platform' && isAllCollide(pageX, pageY, 'platform') && !isCollide(pageX, pageY, 'box');
 }
 
-function generateBox() {
+const isCollide = (x, y, elClassName) => {
+	const corners = prepareCorners(x, y);
+	for (let corner of corners) {
+		let el = document.elementFromPoint(corner[0], corner[1])
+		if (el.className === elClassName) {
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+const isAllCollide = (x, y, elClassName) => {
+	const corners = prepareCorners(x, y);
+	for (let corner of corners) {
+		let el = document.elementFromPoint(corner[0], corner[1])
+		if (el.className !== elClassName) {
+			return false;
+		}
+	}
+	
+	return true
+}
+
+const prepareCorners = (x, y) => {
+	const topLeft = [x, y];
+	const topRight = [x + boxWidth, y];
+	const bottomLeft = [x, y + boxHeight];
+	const bottomRight = [x + boxWidth, y + boxHeight];
+
+	return [topLeft, topRight, bottomLeft, bottomRight];
+}
+
+const moveBox = (layerX, layerY, target) => {
+	const destX = layerX - shiftLeft;
+	const destY = layerY - shiftTop;
+
+	dragged.remove(dragged);
+
+	dragged.style.left = destX + 'px';
+	dragged.style.top = destY + 'px';
+
+	target.appendChild(dragged);
+}
+
+const generateBox = () => {
 	let color = randomColor();
 
 	let boxesDiv = document.getElementById('boxes');
@@ -37,4 +84,8 @@ function generateBox() {
 	div.style = `background-color: #${color}`;
 
 	boxesDiv.appendChild(div);
+}
+
+const  randomColor = () => {
+	return Math.floor(Math.random()*16777215).toString(16);
 }
